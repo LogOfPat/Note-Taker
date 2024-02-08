aa0639e109a1ae78909710fab2446edf2909447f
LogPat <1bagels2@gmail.com>
2024-02-07 18:28:34 -0600

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes')
 
-const PORT = 8005;
+const PORT = process.env.PORT || 8005;
 
 const app = express();
 
